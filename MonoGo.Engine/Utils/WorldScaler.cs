23d9547945e2e1735e2b593585635a7db86220d7
BlizzCrafter <7989230+BlizzCrafter@users.noreply.github.com>
2025-04-02 12:41:57 +0200

    Fix: Reworked the WorldScaler helper class to use GameMgr.WindowManager.CanvasSize as reference for scaling, which is the users selected game resolution. WorldScaleBase is used as the world size.

--- MonoGo.Engine/Utils/WorldScaler.cs
+++ MonoGo.Engine/Utils/WorldScaler.cs
@@ -1,72 +1,86 @@
 ﻿using Microsoft.Xna.Framework;
-using System;
 
 namespace MonoGo.Engine.Utils
 {
+    /// <summary>
+    /// You can use this class to scale the contents of your world in relation to the <c>GameMgr.WindowManager.CanvasSize</c>.
+    /// </summary>
     public static class WorldScaler
     {
+        /// <summary>
+        /// Usually your world map size.
+        /// </summary>
         public static Vector2 WorldScaleBase
         {
             get { return _worldScaleBase; }
-            set
+            private set
             {
                 _worldScaleBase = value;
                 CalculateScalingFactor();
             }
         }
-        private static Vector2 _worldScaleBase = new(1280, 720);
-        public static float WorldScaleFactor { get; set; } = 1f;
+        private static Vector2 _worldScaleBase;
 
-        public static void Init(Vector2 worldScaleBase = default)
+        /// <summary>
+        /// The factor of how much the world scaled based on <c>WorldScaleBase</c>.
+        /// </summary>
+        /// <remarks>Use this value to scale the contents of your world.</remarks>
+        public static Vector2 WorldScaleVector { get; set; } = Vector2.One;
+
+        /// <summary>
+        /// Initialize the WorldScaler for helping scale your world and world-contents.
+        /// </summary>
+        /// <param name="worldScaleBase">Usually your world map size.</param>
+        public static void Init(Vector2 worldScaleBase)
         {
-            WorldScaleBase = worldScaleBase == default ? _worldScaleBase : worldScaleBase;
+            WorldScaleBase = worldScaleBase;
         }
 
         private static void CalculateScalingFactor()
         {
-            WorldScaleFactor = Math.Min(
+            WorldScaleVector = new Vector2(
                 GameMgr.WindowManager.CanvasSize.X / WorldScaleBase.X,
                 GameMgr.WindowManager.CanvasSize.Y / WorldScaleBase.Y);
         }
 
         public static Vector2 WorldToScreen(Vector2 value)
         {
-            return value * WorldScaleFactor;
+            return value * GameMgr.WindowManager.CanvasSize;
         }
 
         public static Vector2 ScreenToWorld(Vector2 value)
         {
-            return value / WorldScaleFactor;
+            return value / GameMgr.WindowManager.CanvasSize;
         }
 
         public static float WorldToScreen(float value)
         {
-            return value * WorldScaleFactor;
+            return value * GameMgr.WindowManager.CanvasSize.X;
         }
 
         public static float ScreenToWorld(float value)
         {
-            return value / WorldScaleFactor;
+            return value / GameMgr.WindowManager.CanvasSize.X;
         }
 
         public static float ToScreen(this float value)
         {
-            return value * WorldScaleFactor;
+            return value * GameMgr.WindowManager.CanvasSize.X;
         }
 
         public static float ToWorld(this float value)
         {
-            return value / WorldScaleFactor;
+            return value / GameMgr.WindowManager.CanvasSize.X;
         }
 
         public static Vector2 ToScreen(this Vector2 value)
         {
-            return value * WorldScaleFactor;
+            return value * GameMgr.WindowManager.CanvasSize;
         }
 
         public static Vector2 ToWorld(this Vector2 value)
         {
-            return value / WorldScaleFactor;
+            return value / GameMgr.WindowManager.CanvasSize;
         }
     }
 }
